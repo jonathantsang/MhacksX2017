debd43faec109bc2aca95411eee251f98efed5ef
jonathantsang <j26tsang@gmail.com>
2017-09-23 08:36:20 -0400

    all

--- api/resolver.js
+++ api/resolver.js
@@ -11,7 +11,11 @@ module.exports = function(req, res) {
 };
 
 
-function handleSearchString(term, req, res){   
-    res.json( {"body": "kys"});
+function handleSearchString(term, req, res){
+	var html = '<img style="max-width:100%;" src="' + "https://www.google.org/assets/static/images/logo_googledotorg-171e7482e5523603fc0eed236dd772d8.svg" + '" width="100%"/>';
+    	res.json({
+      	body: html
+        	// Add raw:true if you're returning content that you want the user to be able to edit
+    	});
     console.log("sent");
 }
